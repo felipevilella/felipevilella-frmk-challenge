06f73f31167b0ef9d63272049341a98a02458c8a
Felipe Vilella <felipevilellaalves@hotmail.com>
2022-02-16 22:05:53 -0300

    feat: structure creation

---
+++ README.md
@@ -0,0 +1,12 @@
+# FRMK Challenge API
+
+### Executando o projeto
+
+  1. Execute ```yarn dev``` raiz da aplicação.
+  2. A aplicação estará disponível no endereço [http://localhost:3333](http://localhost:3333)
+  3. Documentação em swagger [https://localhost:3333/api-docs/](https://localhost:3333/api-docs/))
+
+### Comandos úteis
+  - `yarn build` Gera um build para a produção
+  - `yarn` instalar bibliotecas
+  
---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import '../typeorm';
+import { errors } from 'celebrate';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+
+import { AppError } from '@shared/errors/AppError';
+import { router } from '@shared/infra/http/routes';
+
+import swaggerFile from '../../../swagger.json';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(errors());
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server erro - ${err.message}`,
+    });
+  },
+);
+
+export { app };
